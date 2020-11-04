eead6c813f3675828cb0411d11a148c407df8e49
SonTrungTo <son.trung.to@gmail.com>
2020-11-04 07:26:39 +0200

    adding CSS for FindPeople component. Writing PostMessage now...

--- client/user/FindPeople.js
+++ client/user/FindPeople.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { findPeople, follow } from "./api-user";
+import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
@@ -25,6 +26,26 @@ const useStyles = makeStyles(theme => ({
         margin: 10
     },
     error: {
+        marginRight: theme.spacing(2)
+    },
+    root: theme.mixins.gutters({
+        padding: theme.spacing(1),
+        margin: 0
+    }),
+    title: {
+        color: theme.palette.openTitle,
+        margin: `${theme.spacing(3)}px ${theme.spacing(1.5)}px
+        ${theme.spacing(2)}px`,
+        fontSize: '1.5em',
+        textAlign: 'center',
+        wordSpacing: '0.25em',
+        fontWeight: 'bold'
+    },
+    follow: {
+        right: theme.spacing(2)
+    },
+    viewButton: {
+        verticalAlign: 'middle',
         marginRight: theme.spacing(2)
     }
 }));
@@ -86,51 +107,53 @@ export default function FindPeople() {
 
     return (
         <div>
-            <Typography color="primary" component="h6">
-                Who To Follow
-            </Typography>
-            <List>
-                { values.users.map((user, i) => {
-                    return (
-                        <span key={i}>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar className={ classes.bigAvatar }
-                                    src={ "/api/users/photo/" + user._id } />
-                                </ListItemAvatar>
-                                <ListItemText primary={ user.name } />
-                                <ListItemSecondaryAction className={ classes.follow }>
-                                    <Link to={ "/user/" + user._id }>
-                                        <IconButton variant="contained"
-                                        color="secondary"
-                                        className={ classes.viewButton }>
-                                            <Visibility />
-                                        </IconButton>
-                                    </Link>
-                                    <Button aria-label="Follow"
-                                    color="primary" variant="contained"
-                                    onClick={ () => {clickFollow(user, i)} }>
-                                        Follow
-                                    </Button>
-                                </ListItemSecondaryAction>
-                            </ListItem>
-                        </span>
-                    );
-                }) 
-                }
-                { values.error && (
-                <ListItem>
-                    <ListItemText primary={
-                        <Typography color="error" component="p">
-                            <Icon color="error" className={ classes.error }>
-                                <Error />
-                            </Icon>
-                            { values.error }
-                        </Typography>
-                    } />
-                </ListItem>
-                ) }
-            </List>
+            <Paper elevation={4} className={ classes.root }>
+                <Typography className={ classes.title }>
+                    WHO TO FOLLOW
+                </Typography>
+                <List>
+                    { values.users.map((user, i) => {
+                        return (
+                            <span key={i}>
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        <Avatar className={ classes.bigAvatar }
+                                        src={ "/api/users/photo/" + user._id } />
+                                    </ListItemAvatar>
+                                    <ListItemText primary={ user.name } />
+                                    <ListItemSecondaryAction className={ classes.follow }>
+                                        <Link to={ "/user/" + user._id }>
+                                            <IconButton variant="contained"
+                                            color="secondary"
+                                            className={ classes.viewButton }>
+                                                <Visibility />
+                                            </IconButton>
+                                        </Link>
+                                        <Button aria-label="Follow"
+                                        color="primary" variant="contained"
+                                        onClick={ () => {clickFollow(user, i)} }>
+                                            Follow
+                                        </Button>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            </span>
+                        );
+                    }) 
+                    }
+                    { values.error && (
+                    <ListItem>
+                        <ListItemText primary={
+                            <Typography color="error" component="p">
+                                <Icon color="error" className={ classes.error }>
+                                    <Error />
+                                </Icon>
+                                { values.error }
+                            </Typography>
+                        } />
+                    </ListItem>
+                    ) }
+                </List>
+            </Paper>
             <Snackbar anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'right'
